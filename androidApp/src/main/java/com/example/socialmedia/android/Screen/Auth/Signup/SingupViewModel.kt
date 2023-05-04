8ee1a3e85adc973259813d0177dcc6420210feeb
pranavo72bex <pranav072bex@ioepc.edu.np>
2023-05-04 14:25:45 +0545

    chore : minor change

--- androidApp/src/main/java/com/example/socialmedia/android/Screen/Auth/Signup/SingupViewModel.kt
+++ androidApp/src/main/java/com/example/socialmedia/android/Screen/Auth/Signup/SingupViewModel.kt
@@ -4,11 +4,42 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import domain.useCase.SignupUseCase
+import kotlinx.coroutines.launch
+import com.example.common.util.Result
 
-class SignUpViewModel: ViewModel() {
+class SignUpViewModel(
+    private val signupUseCase: SignupUseCase
+): ViewModel() {
     var uiState by mutableStateOf(SignupUiState())
 
     private  set
+
+
+    fun signUp(){
+        viewModelScope.launch {
+            uiState = uiState.copy(isAuthenticating = true)
+
+            val authResultData = signupUseCase(uiState.email,uiState.username,uiState.password)
+
+            uiState = when(authResultData){
+                is Result.Error -> {
+                    uiState.copy(
+                        isAuthenticating = false,
+                        authError = authResultData.message
+                    )
+                }
+                is Result.Success ->{
+                    uiState.copy(
+                        isAuthenticating = false,
+                        authenticationSucceed = true
+                    )
+                }
+            }
+
+        }
+    }
 
     fun updateUserName(input: String){
         uiState = uiState.copy(username = input)
@@ -25,5 +56,8 @@ class SignUpViewModel: ViewModel() {
 data class  SignupUiState(
     var username: String = "",
     var email: String = "",
-    var password: String = ""
+    var password: String = "",
+    var isAuthenticating: Boolean = false,
+    var authError: String? = null,
+    var authenticationSucceed: Boolean= false,
 )
